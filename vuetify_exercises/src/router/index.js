f0007840963ab546375052f850852e1b74d063bc
Alexander Monakhov <alexander.monakhov@elfo.net>
2019-12-04 14:38:07 +0100

    fixed things

--- vuetify_exercises/src/router/index.js
+++ vuetify_exercises/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Login from '@/views/Login.vue'
+import Dashboard from '@/views/Dashboard.vue'
 
 Vue.use(Router)
 
@@ -19,23 +22,22 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('./views/Login.vue')
+      component: Login
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: () => import('./views/Dashboard.vue')
+      component: Dashboard
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('./views/Signup.vue')
+      component: () => import('@/views/Signup.vue')
     }
   ]
 })
